8ad9d8b0b7be43d66f330ccf982b650d2ac16209
adamkr98 <adamkrynski2@gmail.com>
2024-01-17 17:05:26 +0100

    adding the structure of the calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+const percent = document.querySelector('.perc');
+const ce = document.querySelector('.ce');
+const c = document.querySelector('.c');
+const del = document.querySelector('.del');
+//const 
+const xSqr = document.querySelector('.1/x');
+//const 
+const div = document.querySelector('.div');
+const seven = document.querySelector('.7');
+const eight = document.querySelector('.8');
+const nine = document.querySelector('.9');
+const multi = document.querySelector('.multi');
+const four = document.querySelector('.4');
+const five = document.querySelector('.5');
+const six = document.querySelector('.6');
+const subtr = document.querySelector('.subtr');
+const one = document.querySelector('.2');
+const two = document.querySelector('.3');
+const three = document.querySelector('.7');
+const seven = document.querySelector('.7');
